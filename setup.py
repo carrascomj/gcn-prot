fd0e85cec98a257f157c32f5024c5de6134ef2b6
carrascomj <carrascomurielj@gmail.com>
2020-03-05 14:36:36 +0100

    chore: build skeleton of the project cookiecutterdatascience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Graph convolutional networks tperform structural learning of proteins',
+    author='Jorge CarrascoBjorn Hansen',
+    license='MIT',
+)
