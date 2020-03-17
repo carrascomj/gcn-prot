29b36287535c1b4d1baa680c70690ee1d976ce95
carrascomj <carrascomurielj@gmail.com>
2020-03-17 21:50:24 +0100

    chore: update requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,16 @@ from setuptools import find_packages, setup
 setup(
     name="gcn_prot",
     packages=find_packages(),
-    install_requires=["numpy", "mpi4py", "pandas", "sklearn", "scipy", "wget"],
+    install_requires=[
+        "numpy",
+        "mpi4py",
+        "pandas",
+        "sklearn",
+        "scipy",
+        "torch",
+        "torchvision",
+        "wget",
+    ],
     version="0.1.0",
     description="Graph convolutional networks tperform structural learning of proteins",
     author="Jorge Carrasco and Bjorn Hansen",
