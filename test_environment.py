602f091e143bfe631ded9abb442321d0eccc3458
carrascomj <carrascomurielj@gmail.com>
2020-03-25 12:39:47 +0100

    tests: check environment also with pytest

--- test_environment.py
+++ test_environment.py
@@ -1,25 +1,28 @@
+"""Check that the correct version of python is being used."""
 import sys
 
 REQUIRED_PYTHON = "python3"
 
 
-def main():
+def test_python_version():
+    """Check that the correct version of python is being used."""
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError("Unrecognized python interpreter: {}".format(REQUIRED_PYTHON))
 
     if system_major != required_major:
         raise TypeError(
             "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
+                required_major, sys.version
+            )
+        )
     else:
         print(">>> Development environment passes all tests!")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    test_python_version()
