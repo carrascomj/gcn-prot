61d2583690e85662531d9690624618ed401c7e53
carrascomj <carrascomurielj@gmail.com>
2020-03-25 10:11:14 +0100

    test: test dataloader with adjacency

--- tests/test_data/test_protein_data.py
+++ tests/test_data/test_protein_data.py
@@ -1,6 +1,7 @@
 """Test dataset building and splitting."""
 
 import pytest
+import torch
 
 from gcn_prot.data import get_datasets, get_longest
 
@@ -41,6 +42,29 @@ def test_indexing(data_path):
     prot_dims = [len(tr) for tr in prot]
     # v, c, m, y
     assert prot_dims == [185, 185, 185, 2]
+
+
+def test_dataloading_batch(data_path):
+    """Test transformation of input."""
+    train, _, _ = get_datasets(
+        data_path=data_path,
+        nb_nodes=185,
+        task_type="classification",
+        nb_classes=2,
+        split=None,
+        k_fold=None,
+        seed=1234,
+    )
+    trainloader = torch.utils.data.DataLoader(
+        train, shuffle=False, batch_size=25, drop_last=False
+    )
+    for batch in trainloader:
+        batch_dims = [len(tr) for tr in batch]
+        break
+
+    v = batch[0]
+    assert batch_dims == [25, 25, 25, 2]
+    assert v.shape == torch.Size([25, 185, 29])
 
 
 @pytest.mark.skip(reason="have to figure out how it works.")
--- tests/test_features/test_adjacency.py
+++ tests/test_features/test_adjacency.py
@@ -2,7 +2,8 @@
 
 import torch
 
-from gcn_prot.features import euclidean_dist
+from gcn_prot.data import get_datasets
+from gcn_prot.features import euclidean_dist, transform_input
 
 
 def test_euclidean():
@@ -12,5 +13,22 @@ def test_euclidean():
     return (res == torch.FloatTensor([[0, 1, 2], [1, 0, 1], [2, 1, 0]])).all()
 
 
-if __name__ == "__main__":
-    test_euclidean()
+def test_transform(data_path):
+    """Test transformation of input."""
+    train, _, _ = get_datasets(
+        data_path=data_path,
+        nb_nodes=185,
+        task_type="classification",
+        nb_classes=2,
+        split=None,
+        k_fold=None,
+        seed=1234,
+    )
+    for batch in torch.utils.data.DataLoader(
+        train, shuffle=False, batch_size=25, drop_last=False
+    ):
+        some_batch = batch
+        break
+    input, y = transform_input(some_batch)
+    v, adj = input
+    assert adj.shape == torch.Size([25, 185, 185])
