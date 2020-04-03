35170328169990dc8d19ae1b48cb085588e14953
carrascomj <carrascomurielj@gmail.com>
2020-04-03 13:44:58 +0200

    test: pytest normalization layer

--- gcn_prot/models/layers.py
+++ gcn_prot/models/layers.py
@@ -42,8 +42,9 @@ class GraphConvolution(nn.Module):
             f"{self.__class__.__name__} " f"({self.in_features} -> {self.out_features})"
         )
 
-class NormallizationLayer(nn.Module):
-    """Simple GCN layer.
+
+class NormalizationLayer(nn.Module):
+    """Normalization layer for the adjacency matrix.
 
     Adaped from https://github.com/CrivelliLab/Protein-Structure-DL/
     """
@@ -52,7 +53,7 @@ class NormallizationLayer(nn.Module):
         self, in_features, out_features, dropout=0.1, bias=False, act=F.relu, D=100.0
     ):
         """Initialize layer."""
-        super(NormallizationLayer, self).__init__()
+        super(NormalizationLayer, self).__init__()
         self.weight_feat = out_features
         # Define trainable parameters
         self.weight1 = nn.Linear(in_features, self.weight_feat, bias)
@@ -60,12 +61,12 @@ class NormallizationLayer(nn.Module):
         self.d = D
 
     def forward(self, input):
-        """Pass forward features `v` and sparse adjacency matrix `adj`."""
+        """Normalize sparse adjacency matrix `adj` in terms of `v`."""
         v, adj = input
         v_shape = v.shape
         if len(v_shape) > 2:
-           c1 = self.weight1(v)
-           c2 = self.weight2(v)
-           c = (F.sigmoid(c1+c2)*self.d) + 0.00001 # As to not divide by zero 
-           norm_adj = torch.exp(-((adj*adj)/(2*c*c)))
-           return norm_adj
+            c1 = self.weight1(v)
+            c2 = self.weight2(v)
+            c = (F.sigmoid(c1 + c2) * self.d) + 0.00001  # As to not divide by zero
+            norm_adj = torch.exp(-((adj * adj) / (2 * c * c)))
+            return norm_adj
