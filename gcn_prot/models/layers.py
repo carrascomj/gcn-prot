fix: use normalizationwith dense (no implementation for backprop with sparse mm)

gcn_prot/models/layers.py
@@ -27,11 +27,18 @@ class GraphConvolution(nn.Module):
         """Pass forward features `v` and sparse adjacency matrix `adj`."""
         v, adj = input
         v_shape = v.shape
-        if len(v_shape) > 2:
+        in_batch = len(v_shape) > 2
+        if in_batch and adj.is_sparse:
             # stacked matrices (tensor) to concatenated matrices
             v = torch.cat([matrix for matrix in v])
         # apply each adj_i to each aminoacid_i
-        support = torch.spmm(adj, v)
+        if adj.is_sparse:
+            support = torch.spmm(adj, v)
+        else:
+            if in_batch:
+                support = torch.bmm(adj, v)
+            else:
+                support = torch.mm(adj, v)
         # return to tensor
         support = support.reshape(v_shape)
         output = self.act(self.linear(support))
@@ -69,7 +76,6 @@ class NormalizationLayer(nn.Module):
     def forward(self, input):
         """Normalize sparse adjacency matrix `adj` in terms of `v`."""
         v, adj = input
-        adj = adj.to_dense()
         c1 = self.weight1(v)
         c2 = self.weight2(v)
         if len(c2.shape) > 2:
@@ -77,14 +83,13 @@ class NormalizationLayer(nn.Module):
                 torch.sigmoid(c1.bmm(c2.permute(0, 2, 1))) * self.d
             ) + 0.00001  # As to not divide by zero
             c = 1 / (2 * c * c)
-            c = sparsize(c, self.in_cuda, sparsed=False)
         else:
             c = (
                 torch.sigmoid(c1.mm(c2.T)) * self.d
             ) + 0.00001  # As to not divide by zero
 
         norm_adj = torch.exp(-((adj * adj) * c))
-        return v, norm_adj.to_sparse()
+        return v, norm_adj
 
     def __repr__(self):
         """Stringify as typical torch layer."""