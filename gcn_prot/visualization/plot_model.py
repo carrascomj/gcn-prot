ba4c725ef4156684ef83b99887135082af84aa87
carrascomj <carrascomurielj@gmail.com>
2020-04-14 20:52:40 +0200

    fix: use color blind-friendly plots

--- gcn_prot/visualization/plot_model.py
+++ gcn_prot/visualization/plot_model.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 def plot_epoch(epochs, train_loss, test_loss, epoch):
     """Loss plot, neon colors."""
     # Other Neon colors of the palette -> #13CA91 #3B27BA #E847AE #FF9472
-    plt.plot(epochs, train_loss, color="#13CA91", marker="o", label="Training loss")
-    plt.plot(epochs, test_loss, color="#E847AE", marker="o", label="Validation loss")
+    plt.plot(epochs, train_loss, color="#3B27BA", marker="^", label="Training loss")
+    plt.plot(epochs, test_loss, color="#FF9472", marker="X", label="Validation loss")
     plt.title(
         f"Epoch {epoch}\n"
         f"Train Loss: {train_loss[-1]:.3f}\n"
